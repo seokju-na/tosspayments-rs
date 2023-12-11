b5a5c587a7de0d0588b23016682f3214d6698c6b
seokju-na <seokju.me@gmail.com>
2023-12-11 21:51:48 +0900

    makes tosspayments error more friendly (#3)

--- src/data/error_code.rs
+++ src/data/error_code.rs
@@ -1,7 +1,9 @@
+use convert_case::{Case, Casing};
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[non_exhaustive]
 pub enum ErrorCode {
   AlreadyProcessedPayment,
   ProviderError,
@@ -83,4 +85,24 @@ pub enum ErrorCode {
   FailedDbProcessing,
   FailedCardCompanyResponse,
   IdempotentRequestProcessing,
+}
+
+impl std::fmt::Display for ErrorCode {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}", &format!("{:?}", self).to_case(Case::UpperSnake))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn error_code_display() {
+    assert_eq!(format!("{}", ErrorCode::CommonError), "COMMON_ERROR");
+    assert_eq!(
+      format!("{}", ErrorCode::NotSupportedMethod),
+      "NOT_SUPPORTED_METHOD"
+    );
+  }
 }
